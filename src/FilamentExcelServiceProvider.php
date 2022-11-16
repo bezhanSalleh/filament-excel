b293ba48d9255785f6edb2e0f1345be5844c7b50
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-11-16 13:34:38 +0430

    notification close removed

--- src/FilamentExcelServiceProvider.php
+++ src/FilamentExcelServiceProvider.php
@@ -67,8 +67,7 @@ class FilamentExcelServiceProvider extends ServiceProvider
                     Action::make('download')
                         ->label(__('filament-excel::notifications.download_ready.download'))
                         ->url($url, shouldOpenInNewTab: true)
-                        ->button()
-                        ->close(),
+                        ->button(),
                 ])
                 ->persistent()
                 ->sendToDatabase(auth()->user());
