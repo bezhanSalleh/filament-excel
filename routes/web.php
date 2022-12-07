f9840e202018cbcad07bfba43e6ecb7ba95dd27f
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-12-07 10:17:16 +0430

    fixed buffer issue for downloads

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('filament-excel/{path}', function (string $path) {
+    ob_end_clean();
     return
         response()
             ->download(Storage::disk('filament-excel')->path($path), substr($path, 37))
